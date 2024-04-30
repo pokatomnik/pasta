5cf44f50f59c3ce05d3e7e22826baece94b1c101
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2024-04-30 17:43:01 +0300

    Add transition to pasta screen layout

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 basis-full h-screen overflow-hidden bg-gray-200">
       <TopBar onEncryptClick={() => {}} />
-      <div className="flex flex-1 basis-full justify-center pt-0 pb-0 2xl:pt-4 xl:pt-4 lg:pt-4 2xl:pb-4 xl:pb-4 lg:pb-4">
-        <div className="flex p-4 w-full 2xl:w-1/2 xl:w-1/2 lg:w-3/4 bg-white">
+      <div className="flex flex-1 basis-full justify-center transition-all duration-150 pt-0 pb-0 2xl:pt-4 xl:pt-4 lg:pt-4 2xl:pb-4 xl:pb-4 lg:pb-4">
+        <div className="flex p-4 transition-all duration-150 w-full 2xl:w-1/2 xl:w-1/2 lg:w-3/4 bg-white">
           <Editor data={text} />
         </div>
       </div>
