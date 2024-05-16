3f0f19a93354f623645846b4a20ea81858f57349
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2024-05-16 18:00:14 +0300

    Add sync advice

--- islands/ViewPastaTopBar/ui/ViewPastaTopBar.tsx
+++ islands/ViewPastaTopBar/ui/ViewPastaTopBar.tsx
@@ -1,4 +1,4 @@
-import { Signal } from "@preact/signals";
+import { Signal, useSignal } from "@preact/signals";
 import { Nullable } from "decorate";
 import { Pasta } from "entities/Pasta/model/Pasta.ts";
 import { useCallback } from "preact/hooks";
@@ -9,6 +9,8 @@ import {
   useToastContext,
   withToastController,
 } from "shared/Toast/ui/ToastController.tsx";
+import { BottomSheetDialog } from "shared/BottomSheet/ui/BottomSheetDialog.tsx";
+import { Button } from "shared/Button/ui/Button.tsx";
 
 export default withToastController(function ToastControllerViewPastaTopBar(
   props: Readonly<{
@@ -17,6 +19,15 @@ export default withToastController(function ToastControllerViewPastaTopBar(
 ) {
   const { pastaSignal } = props;
   const showToast = useToastContext();
+  const syncHelpDialogState = useSignal(false);
+
+  const showSyncHelp = useCallback(() => {
+    syncHelpDialogState.value = true;
+  }, []);
+
+  const hideSyncHelp = useCallback(() => {
+    syncHelpDialogState.value = false;
+  }, []);
 
   const handleClick = useCallback(() => {
     if (pastaSignal.value && !pastaSignal.value.e) {
@@ -29,10 +40,38 @@ export default withToastController(function ToastControllerViewPastaTopBar(
   }, [showToast]);
 
   return (
-    <TopBarComponent title={pastaSignal.value?.n ?? ""}>
-      {pastaSignal.value && !pastaSignal.value.e
-        ? <TopBarButton onClick={handleClick}>Copy</TopBarButton>
-        : <></>}
-    </TopBarComponent>
+    <>
+      <TopBarComponent title={pastaSignal.value?.n ?? ""}>
+        <TopBarButton onClick={showSyncHelp}>Sync</TopBarButton>
+        {pastaSignal.value && !pastaSignal.value.e
+          ? <TopBarButton onClick={handleClick}>Copy</TopBarButton>
+          : <></>}
+      </TopBarComponent>
+      <BottomSheetDialog
+        position="bp75"
+        visibility={syncHelpDialogState}
+        title="Show to sync"
+        buttons={
+          <Button type="button" buttonType="primary" onClick={hideSyncHelp}>
+            Ok, I'll try
+          </Button>
+        }
+      >
+        <div className="m-4 flex flex-1 basis-full flex-col overflow-x-hidden overflow-y-auto">
+          <p className="mb-1">
+            Pasta is a completely anonymous application. It doesn't have any
+            accounts, Ie doesn't need a server to create encrypted texts.
+            Therefore, synchronization as a feature is missing in the Pasta web
+            application itself.
+          </p>
+          <p>
+            But despite this, the synchronization option is still available, for
+            this you can save the current page to bookmarks in your browser. If
+            your browser supports synchronization, the text will be available
+            via the link in the bookmark.
+          </p>
+        </div>
+      </BottomSheetDialog>
+    </>
   );
 });
