dae6a0296238a08f493d7bb4bd81f109df230723
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2024-05-20 11:23:22 +0300

    Add theme_color

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,6 +4,7 @@ export default function App({ Component }: PageProps) {
     <html>
       <head>
         <meta charset="utf-8" />
+        <meta name="theme-color" content="#6B7280" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="manifest" href="/manifest.json" />
         <script defer src="/app.js" />
